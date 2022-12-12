f8d4fc905b0885dd57245f6d61f76175f248d4b5
Nick <nicholaslombardo97@gmail.com>
2022-12-12 15:06:48 -0800

    added mins col to sample df, cleaned up joined df cols

--- R/clean_data.R
+++ R/clean_data.R
@@ -15,22 +15,23 @@ clean_data <- function(data_path) {
 
   sample <- sample |>
     mutate(times = as_datetime(times, format = time_format)) |>
-    arrange(times)
+    arrange(times) |>
+    mutate(mins = time_length(times - flow$times[1], unit = 'mins'))
 
 
   joined <- sample |>
     left_join(flow, by = c('times' = 'times'), suffix = c('_sample', '_flow')) |>
     mutate(
-      mins = if_else(
-        is.na(mins),
-        time_length(times - times[which.min(times)], unit = 'mins') + mins[which.min(mins)],
-        mins
-      ),
-      values_flow = if_else(
+      values = if_else(
         is.na(values_flow),
-        map_dbl(mins, estimate_flow, flow = flow),
+        map_dbl(mins_sample, estimate_flow, flow = flow),
         values_flow
-      )
+      ),
+      mins = mins_sample,
+      values_sample = NULL,
+      values_flow = NULL,
+      mins_sample = NULL,
+      mins_flow = NULL
     )
 
   list(flow = flow, sample = sample, joined = joined)
