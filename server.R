8a69eb13b8dc3237e180e909cfacf5811d184a40
Nick <nicholaslombardo97@gmail.com>
2022-12-13 12:35:42 -0800

    made data reactive, added caching for plot and table

--- server.R
+++ server.R
@@ -1,5 +1,37 @@
 server <- function(input, output) {
+  library(dplyr)
+  library(lubridate)
+  library(purrr)
+  library(readxl)
+  library(ggplot2)
+  source('R/clean_data.R', local = TRUE)
+  source('R/calculate_bottle_proportions.R', local = TRUE)
+
+
+  data <- reactive({
+    data_path <- input$file$datapath
+    clean_data(data_path)
+  }) |>
+    bindEvent(input$run_graph)
+
+  observe({
+    xmin <- min(data()$flow$mins)
+    xmax <- max(data()$flow$mins)
+    updateSliderInput(inputId = "range", min = xmin, max = xmax,
+                      value = c(xmin, xmax))
+  })
+
+
   output$hydrograph <- renderPlot({
+    flow <- data()$flow
+    sample <- data()$sample
+    joined <- data()$joined
+
+    ymin <- min(flow$values) - sd(flow$values) # maybe just zero instead?
+    ymax <- max(flow$values) + sd(flow$values)
+    xmin <- min(flow$mins)
+    xmax <- max(flow$mins)
+
     ggplot() +
       geom_point(data = joined, aes(x = mins, y = values, color = 'Sample')) +
       geom_line(data = flow, aes(x = mins, y = values, color = 'Flow')) +
@@ -32,9 +64,14 @@ server <- function(input, output) {
         ymax = ymax,
         alpha = 0.2
       )
-  })
+  }) |>
+    bindCache(data(), input$range)
 
   output$proportions <- renderTable({
+    flow <- data()$flow
+    sample <- data()$sample
+    joined <- data()$joined
+
     flow_filtered <- flow |>
       filter(between(mins, input$range[1], input$range[2]))
 
@@ -47,5 +84,6 @@ server <- function(input, output) {
     proportions <- calculate_bottle_proportions(flow_filtered, sample_filtered, joined_filtered)
     colnames(proportions) <- c('Bottle Number', 'Proportions (mL)')
     proportions
-  }, digits = 1, align = 'c', striped = TRUE)
+  }, align = 'c', striped = TRUE) |>
+    bindCache(data(), input$range)
 }
