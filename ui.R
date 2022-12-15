9e47c6fab966fa93a5705b1523cca04027d0fca9
Nick <nicholaslombardo97@gmail.com>
2022-12-15 12:50:21 -0800

    moved table to sidebar, changed title

--- ui.R
+++ ui.R
@@ -1,5 +1,5 @@
 ui <- fluidPage(
-  titlePanel("EMC Calculator"),
+  titlePanel("Flow-Weighting and Compositing Calculator"),
   sidebarLayout(
     sidebarPanel(
       fileInput(
@@ -7,37 +7,29 @@ ui <- fluidPage(
         multiple = FALSE,
         accept = ".xlsx"
       ),
-      actionButton("run_graph", "Load Hydrograph")
+      actionButton("run_graph", "Load Hydrograph"),
+      conditionalPanel(
+        "input.run_graph",
+        downloadButton("download_data", "Download"),
+        tableOutput("proportions")
+      )
     ),
     mainPanel(
-      fluidRow(
-        column(
-          8,
-          conditionalPanel(
-            "input.run_graph",
-            tabsetPanel(
-              tabPanel(
-                "Hydrograph",
-                plotOutput("hydrograph"),
-                sliderInput(
-                  "range",
-                  "Time range in minutes",
-                  min = 0,
-                  max = 1000,
-                  value = c(0, 1000)
-                )
-              ),
-              id = "tabs"
+      conditionalPanel(
+        "input.run_graph",
+        tabsetPanel(
+          tabPanel(
+            "Flow-Weighting",
+            plotOutput("hydrograph"),
+            sliderInput(
+              "range",
+              "Time range in minutes",
+              min = 0,
+              max = 1000,
+              value = c(0, 1000)
             )
-          )
-        ),
-        column(
-          4,
-          tableOutput("proportions"),
-          conditionalPanel(
-            "input.run_graph",
-            downloadButton("download_data", "Download")
-          )
+          ),
+          id = "tabs"
         )
       )
     )
