3cc88cb32d47e962ab0bb07054d5de6a97ef8703
Nick <nicholaslombardo97@gmail.com>
2022-12-15 16:48:01 -0800

    added reset button, adjusted control flow

--- ui.R
+++ ui.R
@@ -2,30 +2,50 @@ ui <- fluidPage(
   titlePanel("Flow-Weighting and Compositing Calculator"),
   sidebarLayout(
     sidebarPanel(
-      fileInput(
-        "file", "Choose Excel File",
-        multiple = FALSE,
-        accept = ".xlsx"
-      ),
-      actionButton("run_graph", "Load Hydrograph"),
       conditionalPanel(
-        "input.run_graph",
-        downloadButton("download_data", "Download"),
+        "input.submit",
+        actionButton("reset_button", "Upload New Data")
+      ),
+      conditionalPanel(
+        "!input.submit",
+        fileInput(
+          "file", "Choose Excel File",
+          multiple = FALSE,
+          accept = ".xlsx"
+        )
+      ),
+      selectInput(
+        "flow_choices",
+        "Flow Units",
+        c("",
+          "ft³/s",
+          "L/s",
+          "m³/s"),
+        width = "100px"
+      ),
+      conditionalPanel(
+        "input.flow_choices != '' & output.fileUploaded & !input.submit",
+        actionButton("submit", "Submit")
+      ),
+      conditionalPanel(
+        "input.submit",
+        downloadButton("download_data", "Download .csv"),
         tableOutput("proportions")
       )
     ),
     mainPanel(
       conditionalPanel(
-        "input.run_graph",
+        "input.submit",
         tabsetPanel(
           tabPanel(
             "Flow-Weighting",
-            plotOutput("hydrograph"),
+            plotOutput("hydrograph", height = 600),
             sliderInput(
               "range",
               "Time range in minutes",
               min = 0,
               max = 1000,
+              step = 5,
               value = c(0, 1000)
             )
           ),
