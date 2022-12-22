c477fa2279f490e89ec6776a5754ce9edeb0e943
Nick <nicholaslombardo97@gmail.com>
2022-12-22 12:11:20 -0800

    moved flow-weighting ui to server.R for conditionally showing

--- ui.R
+++ ui.R
@@ -1,14 +1,24 @@
 source("R/units.R", local = TRUE)
 
 ui <- fluidPage(
+  tags$head(tags$style('body {font-size: 20px}')),
   titlePanel("Flow-Weighting and Compositing Calculator"),
-  conditionalPanel(
-    "!input.submit",
-    fileInput(
-      "file",
-      "Choose Excel File",
-      multiple = FALSE,
-      accept = ".xlsx"
+  fluidRow(
+    column(
+      12,
+      conditionalPanel(
+        "!input.submit",
+        fileInput(
+          "file",
+          "Choose Excel File",
+          multiple = FALSE,
+          accept = ".xlsx"
+        )
+      ),
+      conditionalPanel(
+        "input.flow_choices != '' & output.fileUploaded & !input.submit",
+        actionButton("submit", "Submit")
+      )
     )
   ),
   fluidRow(
@@ -17,11 +27,15 @@ ui <- fluidPage(
       selectInput(
         "flow_choices",
         "Flow Units",
-        c("", allowed_units)
+        c("", allowed_flow_units)
       ),
       conditionalPanel(
-        "input.flow_choices != '' & output.fileUploaded & !input.submit",
-        actionButton("submit", "Submit")
+        "output.has_conc",
+        selectInput(
+          "conc_choices",
+          "Concentration Units",
+          c("", allowed_concentration_units)
+        )
       )
     ),
     column(
@@ -56,28 +70,15 @@ ui <- fluidPage(
       )
     )
   ),
-  conditionalPanel(
-    "input.submit",
-    tabsetPanel(
-      tabPanel(
-        "Flow-Weighting",
-        fluidRow(
-          column(
-            3,
-            conditionalPanel(
-              "input.submit",
-              downloadButton("download_data", "Download Results.csv"),
-              tableOutput("proportions")
-            )
-          ),
-          column(
-            9,
-            plotOutput("hydrograph", height = 500)
-          )
-        )
-      ),
-      id = "full_page"
-    )
+  tabsetPanel(
+    tabPanel(
+      "Instructions",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      br(),
+      br(),
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    ),
+    id = "full_page"
   )
 )
 
