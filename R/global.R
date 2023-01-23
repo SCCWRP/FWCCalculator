baf4b254308aa9e6c5c59d67f110330fb2350130
Nick <nicholaslombardo97@gmail.com>
2023-01-23 09:48:01 -0800

    removed unnecessary volume conversion functionality

--- R/global.R
+++ R/global.R
@@ -1,19 +1,1 @@
 global_font_size <- 18
-
-
-# global units
-allowed_flow_units <- c("ft\u00B3/s", "L/s", "m\u00B3/s")
-
-conversion_vector <- c(1, 28.3168, 1, 0.0283168, 0.001, 1) # upper triangle conversion factors by column
-
-
-unit_conversion_matrix <- matrix(0, nrow = length(allowed_flow_units), ncol <- length(allowed_flow_units))
-unit_conversion_matrix[upper.tri(unit_conversion_matrix, diag = TRUE)] <- sapply(conversion_vector, function(x) {1/x})
-unit_conversion_matrix <- t(unit_conversion_matrix)
-unit_conversion_matrix[upper.tri(unit_conversion_matrix, diag = TRUE)] <- conversion_vector
-
-rownames(unit_conversion_matrix) <- allowed_flow_units
-colnames(unit_conversion_matrix) <- allowed_flow_units
-
-
-allowed_concentration_units <- c("mg/L")
--- R/volume_conversion.R
+++
@@ -1,6 +0,0 @@
-volume_conversion <- function(data, new_unit) {
-  data$flow$values <- data$flow$values*unit_conversion_matrix[data$units, new_unit]
-  data$joined$values <- data$joined$values*unit_conversion_matrix[data$units, new_unit]
-  data$units <- new_unit
-  data
-}
