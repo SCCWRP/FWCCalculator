dfc49659a7a82b99a6f14aa281d297a710a93094
Nick <nicholaslombardo97@gmail.com>
2023-01-23 09:59:41 -0800

    added explicit namespace references

--- R/estimate_flow.R
+++ R/estimate_flow.R
@@ -3,14 +3,14 @@ estimate_flow <- function(sample_min, flow) {
   # use a linear interpolation to match the sample time with an estimated flow
   prev_flow <- flow |>
     filter(mins < sample_min) |>
-    select(mins, values) |>
-    tail(1) |>
+    select(mins, flow_values) |>
+    utils::tail(1) |>
     as.numeric()
 
   next_flow <- flow |>
     filter(mins > sample_min) |>
-    select(mins, values) |>
-    head(1) |>
+    select(mins, flow_values) |>
+    utils::head(1) |>
     as.numeric()
 
   estimated_flow <- (prev_flow[2]*(next_flow[1] - sample_min) + next_flow[2]*(sample_min - prev_flow[1]))/
