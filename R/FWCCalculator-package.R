d22a4104a8bff5405e810b8035f0127be39692ae
Nick <nicholaslombardo97@gmail.com>
2023-01-23 10:10:12 -0800

    added tidyr and shinyjs imports, cleaned up other imports

--- R/FWCCalculator-package.R
+++ R/FWCCalculator-package.R
@@ -5,17 +5,5 @@
 #' @import dplyr
 #' @import ggplot2
 #' @import shiny
-#' @importFrom cowplot get_legend
-#' @importFrom cowplot get_panel
-#' @importFrom cowplot plot_grid
-#' @importFrom cowplot save_plot
-#' @importFrom lubridate as_datetime time_length
-#' @importFrom purrr map_dbl
-#' @importFrom readxl read_excel excel_sheets
-#' @importFrom reshape2 melt
-#' @importFrom scales breaks_extended rescale
-#' @importFrom stats sd
-#' @importFrom tibble column_to_rownames
-#' @importFrom utils head tail write.csv
 ## usethis namespace: end
 NULL
