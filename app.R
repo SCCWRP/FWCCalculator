d6de9728243e1e6ca6689a8e4aef244a71149d6a
Nick <nicholaslombardo97@gmail.com>
2023-01-31 11:51:07 -0800

    added explanatory comment

--- app.R
+++ app.R
@@ -1,2 +1,6 @@
+# this file used by shinyapps.io to run the app
+# installing the FWCCalculator package and running the FWCCalculatorApp()
+# function will also launch the app
+
 pkgload::load_all(".")
 FWCCalculatorApp()
