c597701a0097e4f0304d587c71e003fe36fe87fe
Nick <nicholaslombardo97@gmail.com>
2023-08-11 13:00:10 -0700

    added correction for fractional seconds issue

--- R/clean_data.R
+++ R/clean_data.R
@@ -1,15 +1,14 @@
 clean_data <- function(flow, sample) {
-
   flow_out <- flow |>
     rename_with(.fn = function(x) "times", .cols = 1) |>
-    mutate(times = lubridate::as_datetime(times)) |>
+    mutate(times = as.POSIXct(trunc(times))) |>
     arrange(times) |>
     mutate(mins = lubridate::time_length(times - times[1], unit = 'mins')) |>
     reshape2::melt(id.vars = c('mins', 'times'), variable.name = "rate", value.name = "flow_values")
 
   sample_out <- sample |>
     rename_with(.fn = function(x) "times", .cols = 1) |>
-    mutate(times = lubridate::as_datetime(times)) |>
+    mutate(times = as.POSIXct(trunc(times))) |>
     arrange(times) |>
     mutate(mins = lubridate::time_length(times - flow_out$times[1], unit = 'mins')) |>
     reshape2::melt(id.vars = c('mins', 'times'), variable.name = "conc", value.name = "conc_values")
